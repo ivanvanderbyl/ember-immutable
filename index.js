6ec981cfe6739c1992c0f901eea432cc570d9592
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-10-26 15:43:40 +0100

    Try converting the module first

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 'use strict';
 
 // var path = require('path');
+var esTranspiler = require('broccoli-babel-transpiler');
 
 module.exports = {
   name: 'immutable',
 
-  // treeForAddon: function() {
+  treeForAddon: function(tree) {
+    var scriptTree = esTranspiler(tree, { stage: 0, browserPolyfill: true });
+
+
   //   var immutablePath = path.join(this.project.addonPackages['ember-immutable'].path, 'node_modules', 'immutable', 'src');
   //   var tree = this.treeGenerator(immutablePath);
-  //   return this._super.treeForAddon.call(this, tree);
-  // }
+    return this._super.treeForAddon.call(this, scriptTree);
+  }
 };
